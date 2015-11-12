45a5c8ef330a36d6d86efe6df66b91a398cbfd77
timyip1 <timyip1@gmail.com>
2015-11-12 14:28:13 -0800

    got timestamps in

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20151112060621) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", 
+    t.datetime "updated_at", 
   end
 
 end
