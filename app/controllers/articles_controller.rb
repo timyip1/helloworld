8e041c5bab17d231636909825277dccf9b568931
timyip1 <timyip1@gmail.com>
2015-11-12 08:52:25 -0800

    exit
    Added list, and some requirements for there to be a few characters in the title of the form.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,37 @@
 class ArticlesController < ApplicationController
 
+# each def refers to creating a page in the articles folder and gives 
+# it some kind of action, for example, all will index, show will show, 
+# create will make a new item... etc. 
+def index
+	@articles = Article.all
+	end
+
+def show
+	@article = Article.find(params[:id])
+end
 
 def new
+	@article = Article.new
   end
  
 def create
   @article = Article.new(article_params)
 
-
-  @article.save
-  redirect_to @article
+  if @article.save
+  	redirect_to @article
+  else
+  	render 'new'
+  end
 end
+
+
+
 
 private
 def article_params
 	params.require(:article) .permit(:title, :text)
 end
 
-#gotta end again
+#gotta end after every def chunk.
 end
