1ad744e60df648debafff0cbd07b6b19ad45b0f6
timyip1 <timyip1@gmail.com>
2015-11-12 13:44:30 -0800

    starting to add destroy functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,49 +1,53 @@
-class ArticlesController < ApplicationController
 
 # each def refers to creating a page in the articles folder and gives 
 # it some kind of action, for example, all will index, show will show, 
 # create will make a new item... etc. 
-def index
-	@articles = Article.all
-	end
-
-def edit
-	@article = Article.find(params[:id])
-end
-
-def show
-	@article = Article.find(params[:id])
-end
-
-def new
-	@article = Article.new
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
   end
  
-def create
-  @article = Article.new(article_params)
-
-  if @article.save
-  	redirect_to @article
-  else
-  	render 'new'
+  def show
+    @article = Article.find(params[:id])
   end
-end
-
-
-def update
-  @article = Article.find(params[:id])
  
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+  def new
+    @article = Article.new
   end
-end
-#why do we need to type end twice? because if / else?
-
-private
-def article_params
-	params.require(:article) .permit(:title, :text)
-end
-
+ 
+  def edit
+    @article = Article.find(params[:id])
+  end
+ 
+  def create
+    @article = Article.new(article_params)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+ 
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+ 
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
